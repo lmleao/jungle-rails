80ecaa4df796e058fb87aa3e8d8adf5e7b02a4d4
Luiza Leao <luiza.leao96@gmail.com>
2024-04-28 01:55:35 -0400

    Create login routes

--- config/routes.rb
+++ config/routes.rb
@@ -17,8 +17,10 @@ Rails.application.routes.draw do
     resources :products, except: [:edit, :update, :show]
   end
 
-  # These routes will be for signup. The first renders a form in the browse, the second will 
-  # receive the form and create a user in our database using the data given to us by the user.
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+  
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
 
